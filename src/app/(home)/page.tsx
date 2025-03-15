3ae1d26a362ffadfe3ee67c61a7f92ae86f29775
김진우 <rlawlsdn263@naver.com>
2025-03-15 15:38:16 +0900

    FEAT: HomePage에 ServiceSection, LocationSection 추가

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,9 @@
-import { HeroSection, IntroSection } from '@/app/(home)/_sections';
+import {
+  HeroSection,
+  IntroSection,
+  ServiceSection,
+  LocationSection,
+} from '@/app/(home)/_sections';
 import s from './HomePage.module.css';
 
 export default function HomePage() {
@@ -9,6 +14,11 @@ export default function HomePage() {
 
       {/* 3. 소개 섹션 */}
       <IntroSection />
+
+      <ServiceSection />
+
+      {/* 4. 위치 섹션 */}
+      <LocationSection />
     </main>
   );
 }

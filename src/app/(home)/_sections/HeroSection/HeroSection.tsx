1403917f463a0c0cc84e7c2eb688c5700c98b2a8
김진우 <rlawlsdn263@naver.com>
2025-03-15 11:52:54 +0900

    REFACTOR: 히어로 배너 관련 코드를 HeroSection으로 분리

---
+++ src/app/(home)/_sections/HeroSection/HeroSection.tsx
@@ -0,0 +1,20 @@
+import s from './HeroSection.module.css';
+
+export default function HeroSection() {
+  return (
+    <section className={s.HeroSection}>
+      <div className={s.hero}>
+        <div className={s.overlay}></div>
+        <div className={s.titleContainer}>
+          <p className={`${s.title} ${s.title1}`}>
+            당신의 <span className={s.strong}>마음</span>을 이해하는
+          </p>
+          <p className={`${s.title} ${s.title2}`}>
+            <span className={s.strong}>경험</span>이 빛나는{' '}
+            <span className={s.strong}>법률 사무소 상무</span>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,22 +1,11 @@
+import { HeroSection } from '@/app/(home)/_sections';
 import s from './HomePage.module.css';
 
 export default function HomePage() {
   return (
     <main className={s.HomePage}>
-      <section className={s.HeroSection}>
-        <div className={s.hero}>
-          <div className={s.overlay}></div>
-          <div className={s.titleContainer}>
-            <p className={`${s.title} ${s.title1}`}>
-              당신의 <span className={s.strong}>마음</span>을 이해하는
-            </p>
-            <p className={`${s.title} ${s.title2}`}>
-              <span className={s.strong}>경험</span>이 빛나는{' '}
-              <span className={s.strong}>법률 사무소 상무</span>
-            </p>
-          </div>
-        </div>
-      </section>
+      {/* 2. 히어로 섹션 */}
+      <HeroSection />
     </main>
   );
 }

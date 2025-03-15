71fef92d082e76a1639dadfeee31b63fc27a065a
김진우 <rlawlsdn263@naver.com>
2025-03-15 12:20:05 +0900

    FEAT: 전역 네비게이션으로 활용될 Navigation 추가

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import s from './Navigation.module.css';
+
+export default function Navigation() {
+  return (
+    <nav className={s.Navigation}>
+      <div className={s.layoutContainer}>
+        <h1 className={s.logo}>
+          <Link href="/">법무법인 상무</Link>
+        </h1>
+        <ul className={s.ul}>
+          <li className={s.li}>대표 변호사 소개</li>
+          <li className={s.li}>주요 업무</li>
+          <li className={s.li}>상담 신청</li>
+          <li className={s.li}>오시는 길</li>
+        </ul>
+      </div>
+    </nav>
+  );
+}

17084dfaf8bfeef50ecaf431dbdac045d775baeb
김진우 <rlawlsdn263@naver.com>
2025-03-15 15:37:06 +0900

    Location 섹션 추가

---
+++ src/app/(home)/_sections/LocationSection/LocationSection.tsx
@@ -0,0 +1,5 @@
+import s from './LocationSection.module.css';
+
+export default function LocationSection() {
+  return <section className={s.LocationSection}>하이</section>;
+}
--- src/app/(home)/_sections/index.ts
+++ src/app/(home)/_sections/index.ts
@@ -1,2 +1,4 @@
 export { default as HeroSection } from './HeroSection/HeroSection';
 export { default as IntroSection } from './IntroSection/IntroSection';
+export { default as LocationSection } from './LocationSection/LocationSection';
+export { default as ServiceSection } from './ServiceSection/ServiceSection';

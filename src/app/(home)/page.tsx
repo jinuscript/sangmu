4f0c810724618e0814ad8d88ac450f256afef218
김진우 <rlawlsdn263@naver.com>
2025-03-15 11:49:20 +0900

    FEAT: HeroSection 기초 작업 완성

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,22 @@
-import styles from './HomePage.module.css';
+import s from './HomePage.module.css';
 
 export default function HomePage() {
-  return <div className={styles.HomePage}></div>;
+  return (
+    <main className={s.HomePage}>
+      <section className={s.HeroSection}>
+        <div className={s.hero}>
+          <div className={s.overlay}></div>
+          <div className={s.titleContainer}>
+            <p className={`${s.title} ${s.title1}`}>
+              당신의 <span className={s.strong}>마음</span>을 이해하는
+            </p>
+            <p className={`${s.title} ${s.title2}`}>
+              <span className={s.strong}>경험</span>이 빛나는{' '}
+              <span className={s.strong}>법률 사무소 상무</span>
+            </p>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }

f4d218d4dcfa0a74710f7beaa2793de13260149b
rlawlsdn263 <rlawlsdn263@naver.com>
2025-03-16 15:46:44 +0900

    BUG: ServiceNavigation 타입에러 해결

--- src/app/(home)/_components/ServiceNavigation/ServiceNavigation.tsx
+++ src/app/(home)/_components/ServiceNavigation/ServiceNavigation.tsx
@@ -1,10 +1,10 @@
-import { useServiceTypeAtom } from '@/app/(home)/_atoms';
-import s from './ServiceNavigation.module.css';
+import { useServiceTypeAtom } from "@/app/(home)/_atoms";
+import s from "./ServiceNavigation.module.css";
 
 export default function ServiceNavigation() {
   const [service, setService] = useServiceTypeAtom();
 
-  const changeService = (type: string) => {
+  const changeService = (type: "가사" | "민사" | "형사") => {
     setService(type);
   };
 
@@ -13,24 +13,24 @@ export default function ServiceNavigation() {
       <ul className={s.ul}>
         <li className={s.li}>
           <button
-            className={`${s.button} ${service === '가사' ? s.active : ''}`}
-            onClick={() => changeService('가사')}
+            className={`${s.button} ${service === "가사" ? s.active : ""}`}
+            onClick={() => changeService("가사")}
           >
             가사
           </button>
         </li>
         <li className={s.li}>
           <button
-            className={`${s.button} ${service === '민사' ? s.active : ''}`}
-            onClick={() => changeService('민사')}
+            className={`${s.button} ${service === "민사" ? s.active : ""}`}
+            onClick={() => changeService("민사")}
           >
             민사
           </button>
         </li>
         <li className={s.li}>
           <button
-            className={`${s.button} ${service === '형사' ? s.active : ''}`}
-            onClick={() => changeService('형사')}
+            className={`${s.button} ${service === "형사" ? s.active : ""}`}
+            onClick={() => changeService("형사")}
           >
             형사
           </button>

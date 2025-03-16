b2e9cc349ac3ed6156b6c5ad4e5c1de83dd0a5f2
김진우 <rlawlsdn263@naver.com>
2025-03-16 10:39:25 +0900

    FEAT: 주요 업무 영역 전환 기능을 수행하는 ServiceNavigation 추가

---
+++ src/app/(home)/_components/ServiceNavigation/ServiceNavigation.tsx
@@ -0,0 +1,41 @@
+import { useServiceTypeAtom } from '@/app/(home)/_atoms';
+import s from './ServiceNavigation.module.css';
+
+export default function ServiceNavigation() {
+  const [service, setService] = useServiceTypeAtom();
+
+  const changeService = (type: string) => {
+    setService(type);
+  };
+
+  return (
+    <nav className={s.ServiceNavigation}>
+      <ul className={s.ul}>
+        <li className={s.li}>
+          <button
+            className={`${s.button} ${service === '가사' ? s.active : ''}`}
+            onClick={() => changeService('가사')}
+          >
+            가사
+          </button>
+        </li>
+        <li className={s.li}>
+          <button
+            className={`${s.button} ${service === '민사' ? s.active : ''}`}
+            onClick={() => changeService('민사')}
+          >
+            민사
+          </button>
+        </li>
+        <li className={s.li}>
+          <button
+            className={`${s.button} ${service === '형사' ? s.active : ''}`}
+            onClick={() => changeService('형사')}
+          >
+            형사
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
+}

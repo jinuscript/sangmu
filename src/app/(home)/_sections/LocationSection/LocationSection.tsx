b3e1c3af154e10cf85741bbf2042fa1b4b2b5336
김진우 <rlawlsdn263@naver.com>
2025-03-16 13:31:22 +0900

    FEAT: 장소를 알려주는 LocationSection 추가

--- src/app/(home)/_sections/LocationSection/LocationSection.tsx
+++ src/app/(home)/_sections/LocationSection/LocationSection.tsx
@@ -1,5 +1,35 @@
+// 'use client';
+
+import Image from 'next/image';
 import s from './LocationSection.module.css';
 
 export default function LocationSection() {
-  return <section className={s.LocationSection}>하이</section>;
+  return (
+    <section className={s.LocationSection}>
+      <div className={s.layoutContainer}>
+        <p className={s.title}>찾아오시는 길</p>
+
+        <div className={s.imgContainer}>
+          <div className={s.overlay}>
+            <p className={s.desc}>네이버 지도로 이동하기</p>
+          </div>
+          <Image
+            className={s.img}
+            src="/home/법률사무소상무.png"
+            alt=""
+            width={1500}
+            height={800}
+          />
+        </div>
+
+        <div className={s.infoContainer}>
+          <p className={s.p}>
+            광주광역시 동구 지산동 714-20 광주지방변호사회관 302호
+          </p>
+          <p className={s.p}>지산동 714-20</p>
+          <p className={s.p}>062-263-8825</p>
+        </div>
+      </div>
+    </section>
+  );
 }

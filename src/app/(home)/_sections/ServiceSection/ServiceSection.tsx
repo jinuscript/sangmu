f5d26987d847d544e780101ac50861d8503ee26d
김진우 <rlawlsdn263@naver.com>
2025-03-16 10:40:38 +0900

    FEAT: ServiceSection 추가

--- src/app/(home)/_sections/ServiceSection/ServiceSection.tsx
+++ src/app/(home)/_sections/ServiceSection/ServiceSection.tsx
@@ -1,10 +1,27 @@
-import { ServiceCard } from '@/app/(home)/_components';
+'use client';
+
+import { useServiceTypeAtom } from '@/app/(home)/_atoms';
+import { ServiceCard, ServiceNavigation } from '@/app/(home)/_components';
+import SERVICE from '@/app/(home)/_data/SERVICE.json';
 import s from './ServiceSection.module.css';
 
 export default function ServiceSection() {
+  const [service] = useServiceTypeAtom();
+
   return (
     <section className={s.ServiceSection}>
-      <ServiceCard />
+      <div className={s.layoutContainer}>
+        <div className={s.ServiceNavigationContainer}>
+          <ServiceNavigation />
+        </div>
+
+        <div className={s.ServiceCardContainer}>
+          {SERVICE[service].map((s, i) => {
+            const { title, desc } = s;
+            return <ServiceCard key={i} title={title} desc={desc} />;
+          })}
+        </div>
+      </div>
     </section>
   );
 }

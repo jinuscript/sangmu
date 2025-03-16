4d3f5e221365adfc023d4e2beee9c037c070051e
김진우 <rlawlsdn263@naver.com>
2025-03-16 13:31:53 +0900

    CHORE: 엔트리 파일에 StrengthSection 추가

--- src/app/(home)/_sections/index.ts
+++ src/app/(home)/_sections/index.ts
@@ -2,3 +2,4 @@ export { default as HeroSection } from './HeroSection/HeroSection';
 export { default as IntroSection } from './IntroSection/IntroSection';
 export { default as LocationSection } from './LocationSection/LocationSection';
 export { default as ServiceSection } from './ServiceSection/ServiceSection';
+export { default as StrengthSection } from './StrengthSection/StrengthSection';

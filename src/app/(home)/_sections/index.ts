7652a8c2e98fb6a5545eb27dd0006b17d40c475b
rlawlsdn263 <rlawlsdn263@naver.com>
2025-03-18 00:33:35 +0900

    UI: /에 ContactSection을 배치함

--- src/app/(home)/_sections/index.ts
+++ src/app/(home)/_sections/index.ts
@@ -1,5 +1,6 @@
-export { default as HeroSection } from './HeroSection/HeroSection';
-export { default as IntroSection } from './IntroSection/IntroSection';
-export { default as LocationSection } from './LocationSection/LocationSection';
-export { default as ServiceSection } from './ServiceSection/ServiceSection';
-export { default as StrengthSection } from './StrengthSection/StrengthSection';
+export { default as ContactSection } from "./ContactSection/ContactSection";
+export { default as HeroSection } from "./HeroSection/HeroSection";
+export { default as IntroSection } from "./IntroSection/IntroSection";
+export { default as LocationSection } from "./LocationSection/LocationSection";
+export { default as ServiceSection } from "./ServiceSection/ServiceSection";
+export { default as StrengthSection } from "./StrengthSection/StrengthSection";
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,27 +1,32 @@
 import {
+  ContactSection,
   HeroSection,
   IntroSection,
   ServiceSection,
   LocationSection,
   StrengthSection,
-} from '@/app/(home)/_sections';
-import s from './HomePage.module.css';
+} from "@/app/(home)/_sections";
+import s from "./HomePage.module.css";
 
 export default function HomePage() {
   return (
     <main className={s.HomePage}>
-      {/* 2. 히어로 섹션 */}
+      {/* 1. 히어로 섹션 */}
       <HeroSection />
 
-      {/* 키 포인트 추가 */}
+      {/* 2. 특징 섹션 */}
       <StrengthSection />
 
       {/* 3. 소개 섹션 */}
       <IntroSection />
 
+      {/* 4. 서비스 섹션 */}
       <ServiceSection />
 
-      {/* 4. 위치 섹션 */}
+      {/* 5. 상담신청 섹션 */}
+      <ContactSection />
+
+      {/* 6. 위치 섹션 */}
       <LocationSection />
     </main>
   );

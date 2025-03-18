8609db4eda2549608acd19d7131ea8c4ddde1d01
rlawlsdn263 <rlawlsdn263@naver.com>
2025-03-18 11:24:03 +0900

    UI: ContactSection UI 완성

--- src/app/(home)/_sections/ContactSection/ContactSection.tsx
+++ src/app/(home)/_sections/ContactSection/ContactSection.tsx
@@ -6,35 +6,39 @@ export default function ContactSection() {
     <section className={s.ContactSection}>
       <LayoutContainer>
         <h2 className={s.title}>상담 문의</h2>
-        <div className={s.InputContainer}>
-          <div className={s.Input}>
-            <label className={s.label} htmlFor="">
-              성함
-            </label>
-            <input className={s.input} type="text" />
+        <form action="">
+          <div className={s.InputContainer}>
+            <div className={s.Input}>
+              <label className={s.label} htmlFor="">
+                성함
+              </label>
+              <input className={s.input} type="text" />
+            </div>
+
+            <div className={s.Input}>
+              <label className={s.label} htmlFor="">
+                휴대폰
+              </label>
+              <input className={s.input} type="text" />
+            </div>
           </div>
 
-          <div className={s.Input}>
+          <div className={s.Textarea}>
             <label className={s.label} htmlFor="">
-              휴대폰
+              문의내용
             </label>
-            <input className={s.input} type="text" />
+            <textarea className={s.textarea} />
           </div>
-        </div>
 
-        <div className={s.Textarea}>
-          <label className={s.label} htmlFor="">
-            문의내용
-          </label>
-          <textarea className={s.textarea} />
-        </div>
           <div className={s.Checkbox}>
             <input type="checkbox" />
             <label htmlFor="">개인정보 수집 및 이용동의</label>
           </div>
+
           <div className={s.buttonContainer}>
             <button className={s.button}>상담 신청</button>
           </div>
+        </form>
       </LayoutContainer>
     </section>
   );

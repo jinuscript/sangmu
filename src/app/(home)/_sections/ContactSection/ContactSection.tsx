56ca6dd78a99d34abd509d7ac29c049aad90ab84
rlawlsdn263 <rlawlsdn263@naver.com>
2025-03-20 00:14:16 +0900

    FEAT: 폼 상태(formData) 추가 및 입력 핸들러(handleChange) 구현
    - useState를 사용하여 name, phone, content, agreement 상태 관리
    - handleChange 함수에서 input과 checkbox 값 변경 처리
    - input과 checkbox의 onChange 이벤트 연결

--- src/app/(home)/_sections/ContactSection/ContactSection.tsx
+++ src/app/(home)/_sections/ContactSection/ContactSection.tsx
@@ -1,7 +1,29 @@
+"use client";
+
+import { useState, ChangeEvent } from "react";
 import { LayoutContainer } from "@/app/(home)/_components";
 import s from "./ContactSection.module.css";
 
 export default function ContactSection() {
+  const [formData, setFormData] = useState({
+    name: "",
+    phone: "",
+    content: "",
+    agreement: false,
+  });
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, type, value } = e.target;
+
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]:
+        type === "checkbox" ? (e.target as HTMLInputElement).checked : value,
+    }));
+  };
+
   return (
     <section className={s.ContactSection}>
       <LayoutContainer>
@@ -12,14 +34,26 @@ export default function ContactSection() {
               <label className={s.label} htmlFor="">
                 성함
               </label>
-              <input className={s.input} type="text" />
+              <input
+                className={s.input}
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+              />
             </div>
 
             <div className={s.Input}>
               <label className={s.label} htmlFor="">
                 휴대폰
               </label>
-              <input className={s.input} type="text" />
+              <input
+                className={s.input}
+                type="text"
+                name="phone"
+                value={formData.phone}
+                onChange={handleChange}
+              />
             </div>
           </div>
 
@@ -27,11 +61,21 @@ export default function ContactSection() {
             <label className={s.label} htmlFor="">
               문의내용
             </label>
-            <textarea className={s.textarea} />
+            <textarea
+              className={s.textarea}
+              name="content"
+              value={formData.content}
+              onChange={handleChange}
+            />
           </div>
 
           <div className={s.Checkbox}>
-            <input type="checkbox" />
+            <input
+              type="checkbox"
+              name="agreement"
+              checked={formData.agreement}
+              onChange={handleChange}
+            />
             <label htmlFor="">개인정보 수집 및 이용동의</label>
           </div>
 

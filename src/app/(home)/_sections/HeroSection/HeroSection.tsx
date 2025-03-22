301d40749ec5f48f41484af8ab1293a41a8ead09
rlawlsdn263 <rlawlsdn263@naver.com>
2025-03-22 23:29:46 +0900

    UI: HeroSection 문구의 font-size가 뷰포트 480px 이하일 때는 7.5vw가 되도록함

--- src/app/(home)/_sections/HeroSection/HeroSection.tsx
+++ src/app/(home)/_sections/HeroSection/HeroSection.tsx
@@ -1,16 +1,17 @@
-import s from './HeroSection.module.css';
+import s from "./HeroSection.module.css";
 
 export default function HeroSection() {
   return (
     <section className={s.HeroSection}>
       <div className={s.hero}>
-        <div className={s.overlay}></div>
+        <div className={s.overlay} />
+
         <div className={s.titleContainer}>
           <p className={`${s.title} ${s.title1}`}>
             당신의 <span className={s.strong}>마음</span>을 이해하는
           </p>
           <p className={`${s.title} ${s.title2}`}>
-            <span className={s.strong}>경험</span>이 빛나는{' '}
+            <span className={s.strong}>경험</span>이 빛나는{" "}
             <span className={s.strong}>법률사무소 상무</span>
           </p>
         </div>

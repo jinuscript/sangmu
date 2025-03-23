085d4e3ed7174e1bad2849543556c93c54938083
rlawlsdn263 <rlawlsdn263@naver.com>
2025-03-23 16:33:42 +0900

    FEAT: 연속적인 이메일 전송을 방지하기 위해 이메일 전송 성공 시에 5분짜리 쿠키 생성

--- src/lib/actions/sendEmail.ts
+++ src/lib/actions/sendEmail.ts
@@ -1,8 +1,21 @@
 "use server";
 
+import { cookies } from "next/headers";
 import nodemailer from "nodemailer";
 
 export default async function sendEmail(formData: FormData) {
+  // 1. 이메일 상담신청 확인
+  const cookieStore = await cookies();
+  const submitted = cookieStore.get("email_submitted");
+
+  if (submitted) {
+    return {
+      success: false,
+      message: "이미 상담 신청이 완료되었습니다. 잠시 후 다시 시도해주세요.",
+    };
+  }
+
+  // 2. 유효성 검사
   const name = formData.get("name");
   const phone = formData.get("phone");
   const content = formData.get("content");
@@ -16,6 +29,7 @@ export default async function sendEmail(formData: FormData) {
     return { success: false, message: "개인정보 동의가 필요합니다" };
   }
 
+  // 3. 이메일 전송
   try {
     const transporter = nodemailer.createTransport({
       service: "Naver",
@@ -37,6 +51,14 @@ export default async function sendEmail(formData: FormData) {
              <p><b>연락처:</b> ${phone}</p>
              <p><b>메시지:</b></p>
              <p>${content}</p>`,
+    });
+
+    // 4. 이메일 전송 성공 시에 연속전송 방지 쿠키 생성
+    cookieStore.set({
+      name: "email_submitted",
+      value: "true",
+      maxAge: 300, // 5분
+      path: "/",
     });
 
     return { success: true, message: "상담신청이 완료됐습니다" };

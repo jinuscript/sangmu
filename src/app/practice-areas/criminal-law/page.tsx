9a788e8fc859707e90bdba9222371f4fedb60a00
rlawlsdn263 <rlawlsdn263@naver.com>
2025-03-26 00:11:08 +0900

    FEAT: /practice-areas/criminal-law 라우트 추가

---
+++ src/app/practice-areas/criminal-law/page.tsx
@@ -0,0 +1,31 @@
+import { PracticeAreasCard } from "@/app/practice-areas/_components";
+import CRIMINAL_LAW_DATA from "@/app/practice-areas/_datas/criminal-law-data.json";
+import s from "./PracticeAreasPage.module.css";
+
+export default async function PracticeAreasPage() {
+  return (
+    <main>
+      {/* 상단 배너 */}
+      <div className={s.banner}>
+        <div className={s.overlay}>
+          <h2 className={s.title}>형사</h2>
+        </div>
+      </div>
+
+      {CRIMINAL_LAW_DATA.map((item, index) => {
+        const { title, content } = item;
+
+        const isEven = index % 2 === 0;
+
+        return (
+          <PracticeAreasCard
+            key={index}
+            type={isEven ? "white" : "gray"}
+            title={title}
+            content={content}
+          />
+        );
+      })}
+    </main>
+  );
+}

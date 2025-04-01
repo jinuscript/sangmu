e40edf5fb530c9442f8d979e38dc8ca08ed0c7b4
rlawlsdn263 <rlawlsdn263@naver.com>
2025-04-01 23:56:45 +0900

    FEAT: next-sitemap 환경설정 추가

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.송지현변호사.kr",
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  changefreq: "weekly",
+  priority: 0.7,
+};

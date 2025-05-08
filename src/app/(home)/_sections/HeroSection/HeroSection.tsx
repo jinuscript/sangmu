ede341604763ea23aee2532c91fe419530c701d9
jinuscript <jinuscript@gmail.com>
2025-05-09 00:19:28 +0900

    UI: 메인페이지 메세지 변경

--- src/app/(home)/_sections/HeroSection/HeroSection.tsx
+++ src/app/(home)/_sections/HeroSection/HeroSection.tsx
@@ -8,11 +8,13 @@ export default function HeroSection() {
 
         <div className={s.titleContainer}>
           <p className={`${s.title} ${s.title1}`}>
-            마음을 이해하는 <span className={s.strong}>변호</span>를
+            의뢰인의 억울함을 <span className={s.strong}>이해</span>하고
+          </p>
+          <p className={`${s.title} ${s.title1}`}>
+            사건을 정확히 <span className={s.strong}>파악</span>하여
           </p>
           <p className={`${s.title} ${s.title2}`}>
-            사건을 정확히 파악하여 <span className={s.strong}>승소</span>로
-            이끄는 <span className={s.strong}>변론</span>
+            <span className={s.strong}>승소</span>로 이끄는 변론
           </p>
           <p className={s.p}>
             상담부터 판결까지, <span className={s.strong}>법률사무소 상무</span>

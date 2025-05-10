11de5f52865917d37f3a950f0a54f45161b2b434
jinuscript <jinuscript@gmail.com>
2025-05-11 00:42:58 +0900

    FEAT: AdminPage에서는 유저 세션을 검토하는 기능 추가

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,63 +1,32 @@
-"use client";
+// app/admin/page.tsx
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
+import { createServerClient } from "@supabase/ssr";
+import { AdminLoginForm } from "./_components";
 
-import { useState } from "react";
-import { supabase } from "@/lib/actions";
-import { useRouter } from "next/navigation";
-import s from "./AdminPage.module.css";
+export default async function AdminPage() {
+  const cookieStore = await cookies();
 
-const AdminPage = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-  const [success, setSuccess] = useState("");
-  const router = useRouter();
-
-  const handleLogin = async (e: React.FormEvent) => {
-    e.preventDefault();
-    setError("");
-    setSuccess("");
-
-    const { data, error } = await supabase.auth.signInWithPassword({
-      email,
-      password,
-    });
-
-    if (error) {
-      setError("로그인 실패: " + error.message);
-    } else {
-      console.log(data);
-      setSuccess("로그인 성공!");
-      router.replace("/admin/dashboard");
-      // 로그인 성공 후 라우팅 또는 관리자 권한 확인 로직 추가
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll: () => cookieStore.getAll(),
+        setAll: () => {},
+      },
     }
-  };
-
-  return (
-    <main className={s.AdminPage}>
-      <form onSubmit={handleLogin}>
-        <div>
-          <label>아이디(이메일)</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>비밀번호</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type="submit">로그인</button>
-
-        {error && <p style={{ color: "red" }}>{error}</p>}
-        {success && <p style={{ color: "green" }}>{success}</p>}
-      </form>
-    </main>
   );
-};
 
-export default AdminPage;
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  console.log(session);
+
+  if (session) {
+    console.log(session);
+    redirect("/admin/dashboard");
+  }
+
+  return <AdminLoginForm />;
+}

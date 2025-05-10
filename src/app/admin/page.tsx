85e61329cb712e2a60e04906238f4e833e11ce91
jinuscript <jinuscript@gmail.com>
2025-05-10 23:50:28 +0900

    FEAT: 관리자 페이지를 위한 AdminPage 추가

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,16 @@
+const AdminPage = () => {
+  return (
+    <form action="">
+      <div>
+        <label htmlFor="">아이디</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label htmlFor="">비밀번호</label>
+        <input type="text" />
+      </div>
+    </form>
+  );
+};
+
+export default AdminPage;

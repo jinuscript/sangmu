faca63e342938b7c2fc3e7a914f64c20c2800e21
jinuscript <jinuscript@gmail.com>
2025-08-17 00:10:11 +0900

    feat: 비밀번호 검증 폼 컴포넌트 추가

---
+++ src/features/post/verify-password/ui/VerifyPasswordForm/VerifyPasswordForm.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import type { ChangeEvent, FormData } from "react";
+
+import { useParams } from "next/navigation";
+import { useVerifyPasswordMutation } from "@/features/post/verify-password/hook";
+import s from "./VerifyPasswordForm.module.css";
+
+export const VerifyPasswordForm = ({ setIsVerified }) => {
+  const [password, setPassword] = useState("");
+
+  const { id } = useParams();
+
+  const { mutate: verifyPassword } = useVerifyPasswordMutation({
+    onSuccess: () => {
+      setIsVerified(true);
+    },
+    onError: (error) => {
+      console.error(error);
+      alert("비밀번호가 일치하지 않습니다.");
+    },
+  });
+
+  const handleSubmit = (e: FormData<HTMLFormElement>) => {
+    e.preventDefault();
+
+    verifyPassword({ id, password });
+  };
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor=""></label>
+        <input type="password" value={password} onChange={handleInputChange} />
+      </div>
+
+      <button>비밀번호 확인</button>
+    </form>
+  );
+};
---
+++ src/features/post/verify-password/ui/index.ts
@@ -0,0 +1,1 @@
+export { VerifyPasswordForm } from "./VerifyPasswordForm/VerifyPasswordForm";

37c65df39dcf02851221f36c92af87ab39cf97f7
jinuscript <jinuscript@gmail.com>
2025-08-22 00:26:19 +0900

    feat: 게시판 페이지 추가

--- src/app/counsel/page.tsx
+++ src/app/counsel/page.tsx
@@ -1,23 +1,28 @@
-"use client";
-
-import { useState } from "react";
 import Link from "next/link";
-import { useFetchPostsQuery } from "@/features/post/fetch-posts/hook";
+import Image from "next/image";
+import { Board } from "@/features/post/fetch-posts/ui";
+
+import s from "./page.module.css";
 
 export default function CounselPage() {
-  const [page, setPage] = useState(1);
-  const { data, isLoading, error } = useFetchPostsQuery({ page });
-
-  if (isLoading) return <main>Loading...</main>;
-
-  if (error) return <main>Error: {error.message}</main>;
-
-  console.log(data);
-
   return (
-    <main>
-      CounselPage
-      <Link href="/counsel/write">글쓰기</Link>
+    <main className={s.CounselPage}>
+      {/* 배너 */}
+      <div className={s.bannerImage}>
+        <div className={s.overlay}>
+          <p className={s.p}>상담 게시판</p>
+        </div>
+      </div>
+      <div className={s.layoutContainer}>
+        {/* 글쓰기 버튼 */}
+        <div className={s.buttonContainer}>
+          <Link className={s.LinkButton} href="/counsel/write">
+            글쓰기
+          </Link>
+        </div>
+        {/* 게시판 */}
+        <Board />
+      </div>
     </main>
   );
 }

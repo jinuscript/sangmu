7c13f6017c2009e1638673fa2220ba2ef2e3213d
jinuscript <jinuscript@gmail.com>
2025-08-23 18:55:41 +0900

    feat: 상세페이지 UI 추가

--- src/app/counsel/[id]/page.tsx
+++ src/app/counsel/[id]/page.tsx
@@ -3,15 +3,22 @@
 import { VerifyPasswordForm } from "@/features/post/verify-password/ui";
 import { PostByIdPage } from "@/features/post/fetch-post-by-id/ui";
 import { useState } from "react";
+import s from "./page.module.css";
 
 const CounselPageById = () => {
   const [isVerified, setIsVerified] = useState(false);
 
-  if (!isVerified) {
-    return <VerifyPasswordForm setIsVerified={setIsVerified} />;
-  }
-
-  return <PostByIdPage />;
+  return (
+    <main className={s.page}>
+      <div className={s.layoutContainer}>
+        {isVerified ? (
+          <PostByIdPage />
+        ) : (
+          <VerifyPasswordForm setIsVerified={setIsVerified} />
+        )}
+      </div>
+    </main>
+  );
 };
 
 export default CounselPageById;

540456d771fea2585c88159e142d1b08110c45f0
jinuscript <jinuscript@gmail.com>
2025-08-23 23:00:35 +0900

    feat: 타입 추가

---
+++ src/entities/post/type/Post.ts
@@ -0,0 +1,13 @@
+export type Post = {
+  id: string;
+  created_at: string;
+  name: string;
+  password: string;
+  title: string;
+  content: string;
+  is_answered: boolean;
+  admin_reply: string;
+  reply_at: string;
+  deleted_at: string;
+  post_number: number;
+};

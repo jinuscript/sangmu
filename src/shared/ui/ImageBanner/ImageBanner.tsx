1dda73b83da29a8ab57218b940e3812df1c308dc
jinuscript <jinuscript@gmail.com>
2025-08-23 21:55:51 +0900

    feat: 페이지 상단 이미지 배너용 공용 컴포넌트 추가

---
+++ src/shared/ui/ImageBanner/ImageBanner.tsx
@@ -0,0 +1,9 @@
+import s from "./ImageBanner.module.css";
+
+export const ImageBanner = () => {
+  return (
+    <section className={s.ImageBanner}>
+      <h1 className={s.h1}>온라인 상담</h1>
+    </section>
+  );
+};

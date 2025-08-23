e43b45afa0994a806d822bb2bbfbe556ddc511ce
jinuscript <jinuscript@gmail.com>
2025-08-24 00:17:31 +0900

    chore: 사용하지 않는 코드 제거

--- src/app/counsel/[id]/page.tsx
+++ src/app/counsel/[id]/page.tsx
@@ -1,23 +1,21 @@
 "use client";
 
-import { VerifyPasswordForm } from "@/features/post/verify-password/ui";
-import { PostByIdPage } from "@/features/post/fetch-post-by-id/ui";
 import { useState } from "react";
-import s from "./page.module.css";
+
+import { PostByIdPage } from "@/features/post/fetch-post-by-id/ui";
+import { VerifyPasswordForm } from "@/features/post/verify-password/ui";
 
 const CounselPageById = () => {
   const [isVerified, setIsVerified] = useState(false);
 
   return (
-    <main className={s.page}>
-      <div className={s.layoutContainer}>
-        {isVerified ? (
-          <PostByIdPage />
-        ) : (
-          <VerifyPasswordForm setIsVerified={setIsVerified} />
-        )}
-      </div>
-    </main>
+    <section>
+      {isVerified ? (
+        <PostByIdPage />
+      ) : (
+        <VerifyPasswordForm setIsVerified={setIsVerified} />
+      )}
+    </section>
   );
 };
 

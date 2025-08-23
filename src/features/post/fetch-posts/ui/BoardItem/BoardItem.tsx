9aa42367d67bf6a82c8dd07ca645514552dff34b
jinuscript <jinuscript@gmail.com>
2025-08-23 23:02:30 +0900

    feat: 이름 숨기기 기능 추가

--- src/features/post/fetch-posts/ui/BoardItem/BoardItem.tsx
+++ src/features/post/fetch-posts/ui/BoardItem/BoardItem.tsx
@@ -1,21 +1,29 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+
+import type { PostItem } from "@/entities/post/type";
+
 import s from "./BoardItem.module.css";
 
-// // 날짜 포맷팅 함수
+// 날짜 포맷팅 함수
 const formatDate = (dateString: string) => {
   const date = new Date(dateString);
   return date.toISOString().split("T")[0];
 };
 
-type TBoardItem = {
-  post_number: number;
-  id: string;
-  is_answered: boolean;
-  name: string;
-  title: string;
-  created_at: string;
+// 이름 모자이크
+const maskName = (nameString: string) => {
+  if (nameString.length <= 2) {
+    // 이름이 2글자 이하면 마지막 글자만 마스킹
+    return nameString[0] + "*";
+  }
+  // 이름이 3글자 이상이면 가운데 글자들을 *로 마스킹
+  return (
+    nameString[0] +
+    "*".repeat(nameString.length - 2) +
+    nameString[nameString.length - 1]
+  );
 };
 
 export const BoardItem = ({
@@ -25,7 +33,7 @@ export const BoardItem = ({
   name,
   title,
   created_at,
-}: TBoardItem) => {
+}: PostItem) => {
   const { push } = useRouter();
   return (
     <tr className={s.BoardItem} onClick={() => push(`/counsel/${id}`)}>
@@ -38,14 +46,14 @@ export const BoardItem = ({
       {/* 답변 상태 */}
       <td className={`${s.td} ${s.status}`}>
         {is_answered ? (
-          <span className={`${s.badge} ${s.active}`}>답변완료</span>
+          <span className={`${s.badge} ${s.active}`}>상담완료</span>
         ) : (
-          <span className={`${s.badge} ${s.inactive}`}>답변대기</span>
+          <span className={`${s.badge} ${s.inactive}`}>상담중</span>
         )}
       </td>
 
       {/* 이름 */}
-      <td className={`${s.td} ${s.name}`}>{name}</td>
+      <td className={`${s.td} ${s.name}`}>{maskName(name)}</td>
 
       {/* 날짜 */}
       <td className={`${s.td} ${s.date}`}>{formatDate(created_at)}</td>

f18f5c000ff842b6a46e20f033325057089687d5
jinuscript <jinuscript@gmail.com>
2025-08-23 23:02:03 +0900

    refactor: Board 컴포넌트 리팩토링

--- src/features/post/fetch-posts/ui/Board/Board.tsx
+++ src/features/post/fetch-posts/ui/Board/Board.tsx
@@ -1,24 +1,31 @@
 "use client";
 
-import { BoardTable, Pagination } from "@/features/post/fetch-posts/ui";
-import s from "./Board.module.css";
 import { useState } from "react";
+
+import { BoardTable, Pagination } from "@/features/post/fetch-posts/ui";
 import { useFetchPostsQuery } from "@/features/post/fetch-posts/hook";
 
+import s from "./Board.module.css";
+
 export const Board = () => {
+  // 페이지네이션
   const [page, setPage] = useState(1);
-  const { data, isLoading, error } = useFetchPostsQuery({ page });
 
-  if (isLoading) return <main>Loading...</main>;
+  // 페이지네이션에 해당하는 게시글 10개 호출
+  const { data } = useFetchPostsQuery({ page });
 
-  if (error) return <main>Error: {error.message}</main>;
-
-  console.log(data);
+  // undefined 에러 방지
+  if (!data || !data.items || !data.totalPages) {
+    return null;
+  }
 
   return (
     <div className={s.Board}>
-      <BoardTable data={data?.items} />
-      <Pagination page={page} setPage={setPage} totalPages={data?.totalPages} />
+      {/* 게시판 */}
+      <BoardTable posts={data.items} />
+
+      {/* 페이지네이션 */}
+      <Pagination page={page} setPage={setPage} totalPages={data.totalPages} />
     </div>
   );
 };

143489ff74a9f9c533d76ddab753bf4f97c15d8e
jinuscript <jinuscript@gmail.com>
2025-08-23 23:01:43 +0900

    chore: CounselPage에 사용하지 않는 코드, 스타일 제거

--- src/app/counsel/page.tsx
+++ src/app/counsel/page.tsx
@@ -1,28 +1,20 @@
 import Link from "next/link";
-import Image from "next/image";
+
 import { Board } from "@/features/post/fetch-posts/ui";
 
 import s from "./page.module.css";
 
 export default function CounselPage() {
   return (
-    <main className={s.CounselPage}>
-      {/* 배너 */}
-      <div className={s.bannerImage}>
-        <div className={s.overlay}>
-          <p className={s.p}>상담 게시판</p>
-        </div>
+    <section>
+      {/* 글쓰기 버튼 */}
+      <div className={s.buttonContainer}>
+        <Link className={s.LinkButton} href="/counsel/write">
+          글쓰기
+        </Link>
       </div>
-      <div className={s.layoutContainer}>
-        {/* 글쓰기 버튼 */}
-        <div className={s.buttonContainer}>
-          <Link className={s.LinkButton} href="/counsel/write">
-            글쓰기
-          </Link>
-        </div>
-        {/* 게시판 */}
-        <Board />
-      </div>
-    </main>
+      {/* 게시판 */}
+      <Board />
+    </section>
   );
 }

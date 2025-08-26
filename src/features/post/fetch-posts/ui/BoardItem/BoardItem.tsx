51cab4404fb1e3fd9a60c6edc9e12f2b4431ea31
jinuscript <jinuscript@gmail.com>
2025-08-26 20:40:46 +0900

    refactor: maskName 함수 제거

--- src/features/post/fetch-posts/ui/BoardItem/BoardItem.tsx
+++ src/features/post/fetch-posts/ui/BoardItem/BoardItem.tsx
@@ -8,20 +8,6 @@ import { formatDate } from "@/shared/utils";
 
 import s from "./BoardItem.module.css";
 
-// 이름 모자이크
-const maskName = (nameString: string) => {
-  if (nameString.length <= 2) {
-    // 이름이 2글자 이하면 마지막 글자만 마스킹
-    return nameString[0] + "*";
-  }
-  // 이름이 3글자 이상이면 가운데 글자들을 *로 마스킹
-  return (
-    nameString[0] +
-    "*".repeat(nameString.length - 2) +
-    nameString[nameString.length - 1]
-  );
-};
-
 export const BoardItem = ({
   post_number,
   id,
@@ -49,7 +35,7 @@ export const BoardItem = ({
       </td>
 
       {/* 이름 */}
-      <td className={`${s.td} ${s.name}`}>{maskName(name)}</td>
+      <td className={`${s.td} ${s.name}`}>{name}</td>
 
       {/* 날짜 */}
       <td className={`${s.td} ${s.date}`}>{formatDate(created_at)}</td>

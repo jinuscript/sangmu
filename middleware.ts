f03f25bfe89a949213aea842bf38749017b519c6
jinuscript <jinuscript@gmail.com>
2025-09-02 13:30:33 +0900

    feat: `admin`와 하위 라우트를 middleware로 라우트 보호를 수행

---
+++ middleware.ts
@@ -0,0 +1,12 @@
+import { type NextRequest } from "next/server";
+import { updateSession } from "@/shared/lib/supabase/middleware";
+
+export async function middleware(request: NextRequest) {
+  return await updateSession(request);
+}
+
+export const config = {
+  matcher: [
+    "/admin/:path*", // /admin 및 하위 경로 모두
+  ],
+};

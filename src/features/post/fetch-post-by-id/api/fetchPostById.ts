d4be9917c64d0ccc6ad4cfd87845006e69cfd14b
jinuscript <jinuscript@gmail.com>
2025-09-02 17:49:54 +0900

    refactor: 상세 게시글을 불러오는 로직 개선

--- src/features/post/fetch-post-by-id/api/fetchPostById.ts
+++ src/features/post/fetch-post-by-id/api/fetchPostById.ts
@@ -1,6 +1,12 @@
+"use server";
+
 import { createSupabaseServerClient } from "@/shared/lib/supabase/server";
 
 export const fetchPostById = async (id: string) => {
   const supabase = await createSupabaseServerClient();
-  return supabase.from("posts").select("*").eq("id", id).single();
+  return supabase
+    .from("posts")
+    .select("title, name, created_at, content, admin_reply")
+    .eq("id", id)
+    .single();
 };
--- src/features/post/fetch-post-by-id/hook/useFetchPostByIdQuery.ts
+++ src/features/post/fetch-post-by-id/hook/useFetchPostByIdQuery.ts
@@ -1,11 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
-import { fetchPostByIdQuery } from "@/features/post/fetch-post-by-id/api";
+import { fetchPostById } from "../api";
 
 export const useFetchPostByIdQuery = (id: string) => {
   return useQuery({
     queryKey: ["post", id],
-    queryFn: async () => {
-      return fetchPostByIdQuery(id);
-    },
+    queryFn: () => fetchPostById(id),
   });
 };

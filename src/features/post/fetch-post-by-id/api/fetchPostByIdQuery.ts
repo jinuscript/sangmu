abe1ed106f9447c31924800b6eec139e5ae25b28
jinuscript <jinuscript@gmail.com>
2025-09-02 13:15:51 +0900

    refactor: fetchPostById를 라우트 핸들러를 사용하지 않고 통신하도록 변경

--- src/features/post/fetch-post-by-id/api/fetchPostByIdQuery.ts
+++
@@ -1,6 +0,0 @@
-import { createSupabaseClient } from "@/shared/lib/supabase/client";
-
-export const fetchPostByIdQuery = async (id: string) => {
-  const supabase = createSupabaseClient();
-  return await supabase.from("posts").select("*").eq("id", id).single();
-};

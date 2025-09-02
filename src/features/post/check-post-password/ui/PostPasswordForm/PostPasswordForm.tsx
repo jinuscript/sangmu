7de664dbad6dc17b6b4bdd1834731f02c7934bb1
jinuscript <jinuscript@gmail.com>
2025-09-02 17:50:14 +0900

    feat: PostPasswordForm 추가

---
+++ src/features/post/check-post-password/ui/PostPasswordForm/PostPasswordForm.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import type { Dispatch, SetStateAction } from "react";
+import { checkPasswordFormAction } from "../../action/checkPasswordFormAction";
+
+type TPostPasswordForm = {
+  id: string;
+  setIsVerified: Dispatch<SetStateAction<boolean>>;
+};
+
+export const PostPasswordForm = ({ id, setIsVerified }: TPostPasswordForm) => {
+  const formAction = async (formData: FormData) => {
+    const password = formData.get("password");
+
+    if (!password) {
+      alert("비밀번호를 입력해주세요!");
+      return;
+    }
+
+    const isValid = await checkPasswordFormAction(id, password);
+
+    if (!isValid) {
+      alert("비밀번호 틀렸습니다!");
+      return;
+    }
+
+    alert("비밀번호가 일치합니다!");
+    setIsVerified(true);
+  };
+
+  return (
+    <form action={formAction}>
+      <input type="text" name="password" />
+      <button>비밀번호 제출</button>
+    </form>
+  );
+};

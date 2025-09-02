059ca0858a8f047cb612826f46f6fffd60cc0638
jinuscript <jinuscript@gmail.com>
2025-09-02 21:25:37 +0900

    feat: 상세페이지 UI 완성

--- src/features/post/fetch-post-by-id/ui/PostByIdPage/PostByIdPage.tsx
+++ src/features/post/fetch-post-by-id/ui/PostByIdPage/PostByIdPage.tsx
@@ -9,15 +9,21 @@ import s from "./PostByIdPage.module.css";
 export const PostByIdPage = ({ id }: { id: string }) => {
   const { data: queryData } = useFetchPostByIdQuery(id);
 
+  if (!queryData) {
+    return null;
+  }
+
   return (
     <main className={s.PostByIdPage}>
       <p className={s.title}>{queryData?.data?.title}</p>
       <div className={s.container}>
         <p className={s.name}>{queryData?.data?.name}</p>
         <div className={s.column} />
-        {/* <p className={s.date}>{formatDate(queryData?.data?.created_at)}</p> */}
+        <p className={s.date}>{formatDate(queryData?.data?.created_at)}</p>
       </div>
+      <div className={s.border} />
       <p className={s.content}>{queryData?.data?.content}</p>
+      <div className={s.border} />
       {queryData?.data?.admin_reply && (
         <p className={s.AdminReply}>{queryData?.data?.admin_reply}</p>
       )}
